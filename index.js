b979e40b17050683aeffd146daf5d1469b02b666
joan <joan.corporan@enaccion-c.cl>
2024-10-23 13:50:39 -0300

    Vercel

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 require("dotenv").config()
 const app= express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT 
 const cors = require("cors")
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
