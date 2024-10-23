764dd8c8167fd69e5d95a751095ee887d9e8df33
joan <joan.corporan@enaccion-c.cl>
2024-10-23 14:15:38 -0300

    Vercel

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ require("dotenv").config()
 const app= express()
 const port = process.env.PORT 
 const cors = require("cors")
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
+/* const swaggerUi = require('swagger-ui-express'); */
+/* const YAML = require('yamljs'); */
 const clientsRoutes = require("./routes/getRoute")
 const { dbConnection } = require("./db/dataBase")
 const verifyToken = require("./auth/vericarToken")
@@ -13,8 +13,8 @@ const registrarse = require("./auth/register")
 const createClient = require("./controller/postClient")
 
 dbConnection()
-const swaggerDocument = YAML.load('./api/openApi.yaml')
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+/* const swaggerDocument = YAML.load('./api/openApi.yaml') */
+/* app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); */
 app.use(express.json());
 app.use(cors())
 app.use(express.static("public"));
@@ -26,5 +26,5 @@ app.use("/api/clients/create", verifyToken, createClient)
 app.use("/api/clients",verifyToken, clientsRoutes)
 app.listen(port,()=>{
     console.log(`Server escuchando en el ${port}`)
-    console.log('Documentación API disponible en http://localhost:3000/api-docs')
+   
 })
