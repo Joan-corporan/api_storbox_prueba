3fb2069184ffaf21866dc9a5b1f3531bae8efbe6
joan <joan.corporan@enaccion-c.cl>
2024-10-23 13:10:02 -0300

    Vercel

--- db/dataBase.js
+++ db/dataBase.js
@@ -1,14 +1,16 @@
 const { Pool } = require('pg');
 
-
-
 const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+})
+
+/* const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME ,
   password: process.env.DB_PASSWORD ,
   port: process.env.DB_PORT
-});
+}); */
 const dbConnection = async () => {
     try {
   
