af576cf39caceb7c69ddb00e356157a49b32cb58
joan <joan.corporan@enaccion-c.cl>
2024-11-04 15:38:22 -0300

    commits

--- routes/getRoute.js
+++ routes/getRoute.js
@@ -6,18 +6,21 @@ const deleteCliente  = require('../controller/deleteCliente');
 const getListaDeUsers = require('../controller/getListaDeUsers');
 const deleteUserOfList = require('../controller/deleteUserOfList');
 const getCliente = require('../controller/getCliente');
+const registrarse = require('../auth/register');
+const checkAdmin = require('../auth/checkAdmin');
 
 
 const router =Router()
 
 router.get("/filtro",getClientePorFiltro)
-router.get("/getUserList",getListaDeUsers)
+router.get("/getUserList",checkAdmin,getListaDeUsers)
 router.get("/getAll", getCliente)
 router.put("/:rut_cliente",updateClient)
 router.post("/create",createClient)
 router.delete("/:rut_cliente",deleteCliente)
-router.delete("/usuarios/:rut",deleteUserOfList)
-
+router.delete("/usuarios/:rut",checkAdmin,deleteUserOfList)
+router.post("/registrarse",checkAdmin, registrarse);
+/* app.post("/api/clients/create", verifyToken, createClient); */
 
 
 
